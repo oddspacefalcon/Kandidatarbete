1ce660cf2d2873d0479e97d9112fec018f090246
Mikkel Opperud <mikkelopp@gmail.com>
2020-02-19 22:06:09 +0100

    Har implementerat MCTS som alpa go zero gör det (gjorde om "guldgruva koden")     Nogra bitar av information:     1. Man använder inte backprop i alpha go algoritmen. Man gör backprop efter man har fått z-parametern     2. z parametern fås genom att använda MCTS flera gånger tills man blir klar med spelet (syndromen är borta)         därmed får man veta om denna var en sucsses eller ej z = -1 eller 1 --> backprop     3. Jag är fortfarande inte klar med denna versionen. Det sista som strular är att få tilbaka värdena som man sprar i dictionaries Ns[s], Nsa[(s,a)] osv         --> tror jag vet vad som är felet men får göra det i morogon.     4. I tillägg till MCTS har jag en fil som heter MCTS_tester.py för att se om allt fungerar som det ska (att den ger ut output i rätt format, om den räknar korrekt i olika metoder osv)     5. Det kan vara strul med att köra de olika filerna från nogot program ifrån source då jag har tagit bort . innan en del imports t.ex: from .util import Actions --> from util import Actions

--- src/MCTS.py
+++ src/MCTS.py
@@ -1,53 +1,194 @@
-#Mikkels kod
+import numpy as np
+from util import Action, Perspective
+from toric_model import Toric_code
+import math
+
+class MCTS():
+    def __init__(self, args, nnet, toric_code):
+
+        '''
+        Problem med denna kod:
+        1. Behöver ha tillgång till 'device' för att kunna köra perspectives igenom nätvärket se #Problem 1 
 
 
-'''>
-Funktioner: 
-experience_replay(criterion, optimizer, batch_size)
-get_reward()
-select_action() --> förändrar toric.ne
-toric.qubit_matrix --> vilken stat man är på()
-toric.generatePerspective --> ger alla perspektiv som man kan ha
-memory osv
-'''
+        '''
+        self.toric = toric_code
+        self.nnet = nnet
+        #memory buffer används ej här
+        #self.memory_buffer = memeory_buffer
+        '''
+        args ska innehålla gridshift variablen från RL
+        '''
+        self.args = args
+        self.root_state = np.copy(self.toric.qubit_matrix)
+        self.actions_taken = []
+        
 
-# main function for the Monte Carlo Tree Search 
-def monte_carlo_tree_search(root): 
-      
-    while resources_left(time, computational power):
-        leaf = traverse(root)  
-        simulation_result = rollout(leaf) 
-        backpropagate(leaf, simulation_result) 
-          
-    return best_child(root) 
-  
-# function for node traversal 
-def traverse(node): 
-    while fully_expanded(node): 
-        #Get best actions instead by calculating the 
-        node = best_uct(node) 
-          
-    # in case no children are present / node is terminal  
-    return pick_univisted(node.children) or node  
-  
-# function for the result of the simulation 
-def rollout(node): 
-    while non_terminal(node): 
-        node = rollout_policy(node) 
-    return result(node)  
-  
-# function for randomly selecting a child node 
-'''Ska vi ändast använda random policy eller ska nätvärket bestämma/nogot imellan?'''
-def rollout_policy(node): 
-    return pick_random(node.children) 
-  
-# function for backpropagation 
-def backpropagate(node, result): 
-    if is_root(node) return
-    node.stats = update_stats(node, result)  
-    backpropagate(node.parent) 
-  
-# function for selecting the best child 
-# node with highest number of visits 
-def best_child(node): 
-    pick child with highest number of visits 
+        self.branches = {} #inte säker på om denna behövs --> använder denna iallafall inte
+
+        #Vet ej om vi ska byta ut dessa?
+        self.Qsa = {}       # stores Q values for s,a (as defined in the paper)
+        self.Nsa = {}       # stores #times edge s,a was visited
+        self.Ns = {}        # stores #times board s was visited
+        self.Ps = {}        # stores initial policy (returned by neural net)
+
+        self.Es = {}        # stores game.getGameEnded ended for board s
+
+        #Denna används inte i våran algoritm ()
+        self.Vs = {}        # stores game.getValidMoves for board s 
+
+    def get_input_perspectives(self):
+        self.toric.syndrom('next_state')
+        return self.toric.generate_perspective(self.args.gridshift, self.toric.next_state)
+    
+    def get_actionprob(self, temp=1):
+        print(self.root_state)
+        for i in range(self.args.nr_simulations):
+            print("nr simulations " + str(i))
+            self.search(np.copy(self.root_state))
+        
+        self.toric.qubit_matrix = self.root_state
+        s = str(self.root_state)
+
+        #Ger alla positioner: [(Q, i, j), (Q, i, j), ... ] för alla perspektiv (i korrekt ordning)
+        perspective_pos = Perspective(*zip(*self.get_input_perspectives())).position
+
+        #actions = [[X, Y, Z], [X, Y, Z], ... ] --> Varje [X, Y, Z] är dessa opperatorer på ett visst perspektiv
+        actions = [[str(Action(p_pos, x)) for x in range(3)] for p_pos in perspective_pos]
+        print("Selarataor")
+        print(self.root_state)
+        print("dada")
+
+        for act in self.actions_taken:
+            ss, aa = act
+            if(ss!=s):
+                print("HOW?!")
+            print(ss)
+            print(self.Nsa[act])
+        print("dad")
+        pi = [[self.Nsa[(s,a)] if (s,a) in self.Nsa else 0 for a in perspective] for perspective in actions]
+        
+        if temp==0:
+            bestA = np.argmax(pi)
+            pi = [0]*len(pi)
+            pi[bestA]=1
+            return pi
+        #pi = [[prob**(1./temp) for prob in perspective] for perspective in pi]
+        print(np.sum(pi))
+        pi = np.array(pi)
+
+        return pi
+
+    def UCBpuct(self, probability_matrix, actions, s):
+        #s = qubit-matrisen
+        #en annan fråga är ochså hur jag representerar actions här. Vanligtvis brukar detta göras med att man räknar ut alla
+        #Borde hitta nogont set datatyp som låter en göra parallela acions på flera state action pairs samtidigt
+        #Borde också eventuellt hitta ett bättre sätt att representera actions på ett bättre sätt
+        current_Qsa = np.array([[self.Qsa[(s,str(a))] if (s, str(a)) in self.Qsa else 0 for a in opperator_actions] for opperator_actions in actions])
+        current_Nsa = np.array([[self.Nsa[(s,str(a))] if (s, str(a)) in self.Nsa else 0 for a in opperator_actions] for opperator_actions in actions])
+        if s not in self.Ns:
+            current_Ns = 0.001
+        else:
+            if self.Ns[s] == 0:
+                current_Ns = 0.001
+            else:
+                current_Ns = self.Ns[s]
+        return current_Qsa + self.args.cpuct*probability_matrix*np.sqrt(current_Ns/(1+current_Nsa))
+
+
+    def search(self, current_qubit_state):
+        
+        #vet ej om denna behövs:
+        self.toric.qubit_matrix = current_qubit_state
+        self.toric.syndrom('next_state')
+
+        s = str(current_qubit_state)
+        if s not in self.Es:
+            if self.toric.terminal_state('next_state') == 1:#then not terminal state
+                self.Es[s] = 0
+            else:
+                if self.toric.eval_ground_sate():
+                    #Trivial loop --> gamestate won!
+                    self.Es[s] = 1 #or the reward 100 i dunno
+                else:
+                    #non trivial loop --> game lost!
+                    self.Es[s] = -1
+        
+        current_Es = self.Es[s]
+        if current_Es != 0:
+            #Backprop?
+            return self.Es[s]
+        array_of_perspectives = self.get_input_perspectives()
+        #gör om från np[Perspective(perspective, position)] --> Perspective([perspective], [position])*
+        perspectives = Perspective(*zip(*array_of_perspectives))
+        input_perspectives = np.array(perspectives.perspective)
+        perspective_pos = np.array(perspectives.position)
+        if s not in self.Ps:
+            #Problem 1
+            #innan denna matas inn i nätvärket behöver denna först konverteras till en torch tensor och stoppas in i decve
+            #sedan autputar nätvärket ochså policyn för detta
+            #Ps ska inte innehålla v!
+            #detta måste vara fel format!
+            '''
+            om output på nnet är [X, Y, Z, v]:
+            out = nnet.feedforward(perspective_input)
+            Ps[s] = [[out[i][j] for j in range(3)] for i in range(len(out))]
+            #genomsnittet av alla v värden för alla perspektiv
+            v = np.sum([out[i][3] for i in range(len(out))])/len(out)
+            '''
+            v, self.Ps[s] = self.nnet.feedforward(input_perspectives)
+            
+            #normalisera
+            self.Ps[s]  = self.Ps[s]/np.sum(self.Ps[s])
+
+            #är nu färdig med episoden
+            self.Ns[s] = 0
+            return v
+
+        #positionen blir här en tuple: type(p_pos) = tuple, INTE array!
+        #inneffektivt sätt att göra det på som jag har förstått
+        actions = [[Action(p_pos, x) for x in range(3)] for p_pos in perspective_pos]
+        UpperConfidence = self.UCBpuct(self.Ps[s], actions, s)
+
+        #indecies_of_action = delat upp i perspektiv behövs inte
+        perspective_index, action_index = np.unravel_index(np.argmax(UpperConfidence), UpperConfidence.shape)
+        best_perspective = array_of_perspectives[perspective_index]
+
+        best_action = Action(best_perspective.position, action_index+1)
+
+        self.toric.current_state = self.toric.next_state
+
+        #Göra om så att vi ändast behöver ha perspektiv här...
+        a = str(best_action)
+        self.actions_taken.append((s,a))
+        
+        if (s,a) in self.Nsa:
+            self.Nsa[(s,a)] +=1
+            print("adding")
+        else:
+            self.Nsa[(s,a)] = 1
+            print("is in")
+        print(self.Nsa[(s,a)])
+        
+        if s in self.Ns:
+            self.Ns[s]+=1
+        else:
+            self.Ns[s]=1
+
+        self.toric.step(best_action)
+        v = self.search(np.copy(self.toric.qubit_matrix))
+
+        if (s,a) in self.Qsa:
+            self.Qsa[(s,a)] = ((self.Nsa[(s,a)]-1)*self.Qsa[(s,a)] + v)/(self.Nsa[(s,a)])
+            print("at it again!")
+
+        else:
+            self.Qsa[(s,a)] = v
+            print(self.Nsa[(s,a)])
+        return v
+    
+    def expansion(self, curent_qubit_state):
+        pass
+    #backprop händer ej förren man har blivit klar med en episod  --> används ej i alpha go's version av algoritmen
+    def backprop(self):
+        pass
